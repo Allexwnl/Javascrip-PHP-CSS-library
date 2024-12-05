d0a72567be43878dc4fb1da1d4c9ce9603a2c5c5
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2024-12-05 11:22:02 +0100

    kleine verandering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div class="container">
+      <h1>Home</h1>
+      <p>Welcome to the Home page</p>
+      <p>Here we give components and clarification about elements/tags of different code languages</p>
+    </div>
   )
 }
 
